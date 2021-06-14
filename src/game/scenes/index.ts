0a940f5a7efe45ba812f1b907655f6fd9ddaaf05
Unintelligent-Nerd <unintelligent.nerd@gmail.com>
2021-06-14 10:43:03 +0800

    Imported the scenes into index.ts

--- src/game/scenes/index.ts
+++ src/game/scenes/index.ts
@@ -1,6 +1,11 @@
 import { BootScene } from './boot-scene';
 import { GameScene } from './game-scene';
+import MenuScene from './MenuScene';
+import PauseScene from './PauseScene';
+import PlayScene from './PlayScene';
+import PreloadScene from './PreloadScene';
+import ScoreScene from './ScoreScene';
 
-const scenes = [BootScene, GameScene];
+const scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
 
 export default scenes;
