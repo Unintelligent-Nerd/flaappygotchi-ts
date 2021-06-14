ffe76dbebf676e01fcf5963fcc47ae96b4208d1b
Unintelligent-Nerd <unintelligent.nerd@gmail.com>
2021-06-14 11:12:40 +0800

    Edited index.ts

--- src/game/scenes/index.ts
+++ src/game/scenes/index.ts
@@ -6,6 +6,35 @@ import PlayScene from './PlayScene';
 import PreloadScene from './PreloadScene';
 import ScoreScene from './ScoreScene';
 
-const scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
+const WIDTH = 400;
+const HEIGHT = 600;
+const GOTCHI_POSITION = {
+  x: WIDTH / 10, y: HEIGHT /2
+};
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: GOTCHI_POSITION
+}
+
+const scenes = [BootScene, PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
+const createScene = Scene => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
 
 export default scenes;
+
+const config = {
+    type: Phaser.AUTO,
+    ...SHARED_CONFIG,
+    pixelArt: true,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        // debug: true
+      }
+    },
+    scene: initScenes();
+  }
+  
+  new Phaser.Game(config);
